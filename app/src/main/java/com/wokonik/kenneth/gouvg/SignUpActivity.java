f1b2211156c5790f90ad1604fe2d75e1350a4500
Kenneth Martínez <kennethandremm@gmail.com>
2016-08-25 09:48:07 -0600

    ADDED - new list of Strings to es and eng lenguaje and intent to login activity from signup activity

--- app/src/main/java/com/wokonik/kenneth/gouvg/SignUpActivity.java
+++ app/src/main/java/com/wokonik/kenneth/gouvg/SignUpActivity.java
@@ -1,7 +1,9 @@
 package com.wokonik.kenneth.gouvg;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,5 +18,16 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void click(View v) {
+        Intent intent;
+        switch(v.getId()) {
+            case R.id.link_login:
+                intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                break;
+        }
+
     }
 }
