ebf31f13fecc58f059e449cf4b1ae54a2c1bef64
Kenneth Martínez <kennethandremm@gmail.com>
2016-08-24 22:11:43 -0600

    Added icon app

--- app/src/main/java/com/wokonik/kenneth/gouvg/LoginActivity.java
+++ app/src/main/java/com/wokonik/kenneth/gouvg/LoginActivity.java
@@ -1,9 +1,15 @@
 package com.wokonik.kenneth.gouvg;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,20 +17,30 @@ public class LoginActivity extends AppCompatActivity {
     private Button signUpButton;
     private EditText username;
     private EditText password;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        loginButton = (Button)findViewById(R.id.btnLogin);
+        /*loginButton = (Button)findViewById(R.id.btnLogin);
         signUpButton = (Button)findViewById(R.id.btnRegist);
         username = (EditText)findViewById(R.id.editText);
         password = (EditText)findViewById(R.id.passText);
-
-        private FirebaseAuth mAuth;
-// ...
         mAuth = FirebaseAuth.getInstance();
+*/
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
     }
 }
--- app/src/main/java/com/wokonik/kenneth/gouvg/SignUpActivity.java
+++ app/src/main/java/com/wokonik/kenneth/gouvg/SignUpActivity.java
@@ -2,8 +2,15 @@ package com.wokonik.kenneth.gouvg;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private Button signUpButton;
+    private EditText username;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
