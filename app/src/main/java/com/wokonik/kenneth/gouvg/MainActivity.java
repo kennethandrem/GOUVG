706a81b3b3ebaf51f1a50f7abcf80d7879c06f93
kenneth <kennethandremm@gmail.com>
2016-09-30 12:21:31 -0600

    Commit para entrega preliminar 1 POO

--- app/src/main/java/com/wokonik/kenneth/gouvg/MainActivity.java
+++ app/src/main/java/com/wokonik/kenneth/gouvg/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wokonik.kenneth.gouvg;
 
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,6 +8,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,78 +18,140 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.mapbox.mapboxsdk.MapboxAccountManager;
 import com.mapbox.mapboxsdk.annotations.Icon;
 import com.mapbox.mapboxsdk.annotations.IconFactory;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
+import com.mapbox.mapboxsdk.annotations.PolylineOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.services.Constants;
+import com.mapbox.services.commons.ServicesException;
+import com.mapbox.services.commons.geojson.LineString;
+import com.mapbox.services.commons.models.Position;
+import com.mapbox.services.directions.v5.DirectionsCriteria;
+import com.mapbox.services.directions.v5.MapboxDirections;
+import com.mapbox.services.directions.v5.models.DirectionsResponse;
+import com.mapbox.services.directions.v5.models.DirectionsRoute;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private MapView mapView;
+    private MapboxMap map;
+    private DirectionsRoute currentRoute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        MapboxAccountManager.start(this, "pk.eyJ1Ijoia2VubmV0aGFuZHJlbW0iLCJhIjoiY2lydzhrNnYwMGk1dnQybmttZHNxYm5raSJ9.cyodsY5rk-7Fz8WkfkUDmw");
+
+        // Mapbox access token only needs to be configured once in your app
+        MapboxAccountManager.start(this,  "pk.eyJ1Ijoia2VubmV0aGFuZHJlbW0iLCJhIjoiY2lydzhrNnYwMGk1dnQybmttZHNxYm5raSJ9.cyodsY5rk-7Fz8WkfkUDmw");
+
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Alhambra landmark in Granada, Spain.
+        final Position origin = Position.fromCoordinates(14.603554,-90.4913727);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+        // Plaza del Triunfo in Granada, Spain.
+        final Position destination = Position.fromCoordinates(14.6033338,-90.5046841);
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-
+        // Setup the MapView
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(MapboxMap mapboxMap) {
+                map = mapboxMap;
 
-                // Add a basic marker view
-                mapboxMap.addMarker(new MarkerViewOptions()
-                        .position(new LatLng(14.6220757,-90.5166444))
-                );
+                // Add origin and destination to the map
+                mapboxMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(origin.getLatitude(), origin.getLongitude()))
+                        .title("Origin")
+                        .snippet("Alhambra"));
+                mapboxMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(destination.getLatitude(), destination.getLongitude()))
+                        .title("Destination")
+                        .snippet("Plaza del Triunfo"));
 
-                IconFactory iconFactory = IconFactory.getInstance(MainActivity.this);
-                Drawable iconDrawable = ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_menu_send);
-                Icon icon = iconFactory.fromDrawable(iconDrawable);
-
-                // Add a marker view using all of the options available
-                mapboxMap.addMarker(new MarkerViewOptions()
-                        .position(new LatLng(14.6053796, -90.488322))
-                        .icon(icon)
-                        .rotation(90)
-                        .anchor(0.5f, 0.5f)
-                        .alpha(0.5f)
-                        .title("Tu destino")
-                        .infoWindowAnchor(0.5f, 0.5f)
-                        .flat(true));
+                // Get route from API
+                try {
+                    getRoute(origin, destination);
+                } catch (ServicesException e) {
+                    e.printStackTrace();
+                }
             }
         });
-
-
     }
+
+    private final static String TAG = "MainActivity";
+
+    private void getRoute(Position origin, Position destination) throws ServicesException {
+
+        MapboxDirections client = new MapboxDirections.Builder()
+                .setOrigin(origin)
+                .setDestination(destination)
+                .setProfile(DirectionsCriteria.PROFILE_CYCLING)
+                .setAccessToken("pk.eyJ1Ijoia2VubmV0aGFuZHJlbW0iLCJhIjoiY2lydzhrNnYwMGk1dnQybmttZHNxYm5raSJ9.cyodsY5rk-7Fz8WkfkUDmw")
+                .build();
+
+        client.enqueueCall(new Callback<DirectionsResponse>() {
+            @Override
+            public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                // You can get the generic HTTP info about the response
+                Log.d(TAG, "Response code: " + response.code());
+                if (response.body() == null) {
+                    Log.e(TAG, "No routes found, make sure you set the right user and access token.");
+                    return;
+                }
+
+                // Print some info about the route
+                currentRoute = response.body().getRoutes().get(0);
+                Log.d(TAG, "Distance: " + currentRoute.getDistance());
+                Toast.makeText(MainActivity.this, "Route is " +  currentRoute.getDistance() + " meters long.", Toast.LENGTH_SHORT).show();
+
+                // Draw the route on the map
+                drawRoute(currentRoute);
+            }
+
+            @Override
+            public void onFailure(Call<DirectionsResponse> call, Throwable t) {
+                Log.e(TAG, "Error: " + t.getMessage());
+                Toast.makeText(MainActivity.this, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void drawRoute(DirectionsRoute route) {
+        // Convert LineString coordinates into LatLng[]
+        LineString lineString = LineString.fromPolyline(route.getGeometry(), Constants.OSRM_PRECISION_V5);
+        List<Position> coordinates = lineString.getCoordinates();
+        LatLng[] points = new LatLng[coordinates.size()];
+        for (int i = 0; i < coordinates.size(); i++) {
+            points[i] = new LatLng(
+                    coordinates.get(i).getLatitude(),
+                    coordinates.get(i).getLongitude());
+        }
+
+        // Draw Points on MapView
+        map.addPolyline(new PolylineOptions()
+                .add(points)
+                .color(Color.parseColor("#009688"))
+                .width(5));
+    }
+
 
     @Override
     public void onBackPressed() {
--- app/src/main/java/com/wokonik/kenneth/gouvg/SignUpActivity.java
+++ app/src/main/java/com/wokonik/kenneth/gouvg/SignUpActivity.java
@@ -1,32 +1,228 @@
 package com.wokonik.kenneth.gouvg;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    private Button loginButton;
     private Button signUpButton;
+    private EditText studentId;
     private EditText username;
+    private EditText email;
     private EditText password;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "EmailPassword";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //initialize local variables for manage input data
+        studentId = (EditText)findViewById(R.id.input_id);
+        username = (EditText)findViewById(R.id.input_name);
+        email = (EditText)findViewById(R.id.input_email);
+        password = (EditText)findViewById(R.id.input_password);
+
+        signUpButton = (Button)findViewById(R.id.btn_signup);
+
+        // [START initialize_auth]
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+
+        // [START auth_state_listener]
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // [START_EXCLUDE]
+                // [END_EXCLUDE]
+            }
+        };
+        // [END auth_state_listener]
     }
+
+    // [START on_start_add_listener]
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    // [END on_start_add_listener]
+
+    // [START on_stop_remove_listener]
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    // [END on_stop_remove_listener]
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressDialog();
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(SignUpActivity.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // [START_EXCLUDE]
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressDialog();
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Intent intent;
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(SignUpActivity.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(SignUpActivity.this, "Creado exitosamente, ahora inicia sesion",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        Toast.makeText(SignUpActivity.this, "Sesion cerrada",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String emailt = email.getText().toString();
+        if (TextUtils.isEmpty(emailt)) {
+            email.setError("Required.");
+            valid = false;
+        } else {
+            email.setError(null);
+        }
+
+        String passwordt = password.getText().toString();
+        if (TextUtils.isEmpty(passwordt)) {
+            password.setError("Required.");
+            valid = false;
+        } else {
+            password.setError(null);
+        }
+
+        return valid;
+    }
+
 
     public void click(View v) {
         Intent intent;
+        String emailt = email.getText().toString();
+        if (TextUtils.isEmpty(emailt)) {
+            email.setError("Required.");
+        } else {
+            email.setError(null);
+        }
+
+        String passwordt = password.getText().toString();
+        if (TextUtils.isEmpty(passwordt)) {
+            password.setError("Required.");
+        } else {
+            password.setError(null);
+        }
         switch(v.getId()) {
             case R.id.link_login:
-                intent = new Intent(this, LoginActivity.class);
+                intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.btn_signup:
+                signIn(emailt, passwordt);
+                intent = new Intent(this, MainActivity.class);
         }
 
     }
